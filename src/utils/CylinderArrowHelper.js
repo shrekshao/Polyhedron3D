dc8de4b6c13768aecf8f8027627e5ea735617543
shrekshao <shrekshao@gmail.com>
2017-02-20 14:44:07 -0500

    fixed length cone of arrow

--- src/utils/CylinderArrowHelper.js
+++ src/utils/CylinderArrowHelper.js
@@ -10,11 +10,13 @@ function createCylinderArrowMesh(pointX, pointY, material, radius, radiusCone, e
         radius = l * 0.01;
     }
 
+    // fixedConeLength = fixedConeLength !== undefined ? fixedConeLength : 4;
+
     if (radiusCone === undefined) {
         radiusCone = 2 * radius;
     }
 
-    edgeLengthRatio = edgeLengthRatio !== undefined ? edgeLengthRatio : 0.7 ;
+    // edgeLengthRatio = edgeLengthRatio !== undefined ? edgeLengthRatio : 0.7 ;
 
     var pointMid = new THREE.Vector3().addVectors(pointX, edgeLengthRatio * direction);
 
@@ -25,16 +27,25 @@ function createCylinderArrowMesh(pointX, pointY, material, radius, radiusCone, e
         0, -1, 0, 0,
         0, 0, 0, 1));
 
+    var edgeGeometry;
+    var coneGeometry;
+    if (edgeLengthRatio !== undefined) {
+        edgeGeometry = new THREE.CylinderGeometry(radius, radius, edgeLengthRatio * l, 8, 1);
+        coneGeometry = new THREE.CylinderGeometry(0, radiusCone, (1-edgeLengthRatio) * l, 8, 1);
+        edgeGeometry.translate( 0,  -(0.5 - 0.5 * edgeLengthRatio) * l, 0 );
+        var translate = new THREE.Matrix4().makeTranslation( 0,  (0.5 - 0.5 * (1 - edgeLengthRatio)) * l, 0 );
+        edgeGeometry.merge(coneGeometry, translate);
+    } else {
+        // fixed length cone
+        var fixedConeLength = 1;
+        edgeGeometry = new THREE.CylinderGeometry(radius, radius, l - fixedConeLength, 8, 1);
+        coneGeometry = new THREE.CylinderGeometry(0, radiusCone, fixedConeLength, 8, 1);
+        edgeGeometry.translate( 0, - 0.5 * fixedConeLength, 0 );
+        var translate = new THREE.Matrix4().makeTranslation( 0, 0.5 * (l - fixedConeLength), 0 );
+        edgeGeometry.merge(coneGeometry, translate);
+    }
+
     
-
-    var edgeGeometry = new THREE.CylinderGeometry(radius, radius, edgeLengthRatio * l, 8, 1);
-    var coneGeometry = new THREE.CylinderGeometry(0, radiusCone, (1-edgeLengthRatio) * l, 8, 1);
-
-    edgeGeometry.translate( 0,  -(0.5 - 0.5 * edgeLengthRatio) * l, 0 )
-
-    var translate = new THREE.Matrix4().makeTranslation( 0,  (0.5 - 0.5 * (1 - edgeLengthRatio)) * l, 0 );
-
-    edgeGeometry.merge(coneGeometry, translate);
 
     var arrow = new THREE.Mesh(edgeGeometry, material);
 
